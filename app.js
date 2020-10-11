ba10a475ee9f806fa7e59fd54777873a8f5209de
kings10-hiroki <h61147366@gmail.com>
2020-10-11 12:30:42 +0900

    Added comment again

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
